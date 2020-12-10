d478a08d61649dd73f2d586672e8eeae496ba10a
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2020-12-09 20:44:42 -0500

    Update day4.py

--- day4.py
+++ day4.py
@@ -11,3 +11,19 @@ def advent4_1():
         else:
             pass
     print(valid)
+
+# i did not write this next part, woe is me
+# I think you need an extra new line at the end of your file
+# part 2
+REGEX = [
+    r"byr:(?:19[2-9]\d|200[0-2])\b",
+    r"iyr:20(?:1\d|20)\b",
+    r"eyr:20(?:2\d|30)\b",
+    r"hgt:(?:1(?:[5-8]\d|9[0-3])cm|(?:59|6\d|7[0-6])in)\b",
+    r"hcl:#[0-9a-f]{6}\b",
+    r"ecl:(amb|blu|brn|gry|grn|hzl|oth)\b",
+    r"pid:\d{9}\b",
+]
+with open("pp.txt") as f:
+    lines = f.read()[:-1].split("\n\n")
+    print(sum(all(re.search(x, s) for x in REGEX) for s in lines))
